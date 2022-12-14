3ea93cd16dbcd54310af7ed41595d9e73cc5390b
Daniel Francisco <76452190+danyjf@users.noreply.github.com>
2022-12-14 16:53:40 +0000

    Add player

--- game.py
+++ game.py
@@ -1,33 +1,47 @@
 ﻿import pygame
 
+from player import Player
+
 class Game:
     def __init__(self, width, height):
         self.display = pygame.display.set_mode((width, height))
         self.clock = pygame.time.Clock()
 
         self.running = True
-        self.game_objects = []
+        self.entities = [Player()]
     
     def loop(self):
         while self.running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
-            
+            self.handle_events()
+            self.process_input()
             self.update()
-            
             self.render()
-            
             self.clock.tick(15)
 
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+    def process_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            pass
+        elif keys[pygame.K_DOWN]:
+            pass
+        elif keys[pygame.K_LEFT]:
+            pass
+        elif keys[pygame.K_RIGHT]:
+            pass
+    
     def update(self):
-        for obj in self.game_objects:
-            obj.update()
+        for entity in self.entities:
+            entity.update()
     
     def render(self):
         self.display.fill("white")
         
-        for obj in self.game_objects:
-            obj.render(self.display)
+        for entity in self.entities:
+            entity.render(self.display)
         
         pygame.display.flip()
---
+++ player.py
@@ -0,0 +1,12 @@
+﻿import pygame
+
+class Player:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+    
+    def update(self):
+        pass
+    
+    def render(self, display):
+        pygame.draw.rect(display, 'red', (self.x, self.y, 32, 32))
