1b5ed9cdc652a6d498f8cbbd7cc1a972af54f37a
Daniel Francisco <76452190+danyjf@users.noreply.github.com>
2022-12-14 16:06:06 +0000

    Display window

---
+++ game.py
@@ -0,0 +1,33 @@
+﻿import pygame
+
+class Game:
+    def __init__(self, width, height):
+        self.display = pygame.display.set_mode((width, height))
+        self.clock = pygame.time.Clock()
+
+        self.running = True
+        self.game_objects = []
+    
+    def loop(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+            
+            self.update()
+            
+            self.render()
+            
+            self.clock.tick(15)
+
+    def update(self):
+        for obj in self.game_objects:
+            obj.update()
+    
+    def render(self):
+        self.display.fill("white")
+        
+        for obj in self.game_objects:
+            obj.render(self.display)
+        
+        pygame.display.flip()
---
+++ main.py
@@ -0,0 +1,11 @@
+﻿import pygame
+
+from game import Game
+
+if __name__ == "__main__":
+    pygame.init()
+    
+    game = Game(width=500, height=500)
+    game.loop()
+    
+    pygame.quit()
